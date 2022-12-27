69344aa743ba4344f756370b025ccc21c09af81d
jeonbyeongmin <qudals7613@gmail.com>
2022-12-27 21:57:44 +0900

    feat: Apply `Layout`

---
+++ pages/parties/[party-slug]/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PartyLayout from 'components/layouts/PartyLayout';
+
+function PartyPage() {
+  return <PartyLayout>PartyPage</PartyLayout>;
+}
+
+export default PartyPage;
---
+++ pages/parties/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PartiesPage() {
+  return <div>PartiesPage</div>;
+}
+
+export default PartiesPage;
