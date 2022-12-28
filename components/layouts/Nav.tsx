bbdd5c129bb223ea60368577efa5fa60c9339227
jeonbyeongmin <qudals7613@gmail.com>
2022-12-28 17:06:04 +0900

    minor: change some minor code

--- components/layouts/Nav.tsx
+++ components/layouts/Nav.tsx
@@ -1,38 +1,42 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link, List, ListItem, Text } from 'components/primitive';
 import { styled } from 'stitches.config';
 import { Avatar } from 'components/Avatar';
+import { useRouter } from 'next/router';
 
 export function Nav() {
+  const router = useRouter();
+  console.log(router.asPath);
+
   return (
     <NavWrapper>
       <List>
         <Link href={'/parties/1'}>
-          <ListItem active>
+          <ListItem active={router.asPath === '/parties/1'}>
             <Avatar size="2" shape="square" src="https://i.pravatar.cc/300" />
             <Text size={'2'}>프론트엔드 정보</Text>
           </ListItem>
         </Link>
         <Link href={'/parties/2'}>
-          <ListItem>
+          <ListItem active={router.asPath === '/parties/2'}>
             <Avatar size="2" shape="square" src="https://i.pravatar.cc/300" />
             <Text size={'2'}>파티 이름 입니다</Text>
           </ListItem>
         </Link>
         <Link href={'/parties/3'}>
-          <ListItem>
+          <ListItem active={router.asPath === '/parties/3'}>
             <Avatar size="2" shape="square" src="https://i.pravatar.cc/300" />
             <Text size={'2'}>파티 이름 입니다</Text>
           </ListItem>
         </Link>
         <Link href={'/parties/4'}>
-          <ListItem>
+          <ListItem active={router.asPath === '/parties/4'}>
             <Avatar size="2" shape="square" src="https://i.pravatar.cc/300" />
             <Text size={'2'}>파티 이름 입니다</Text>
           </ListItem>
         </Link>
         <Link href={'/parties/5'}>
-          <ListItem>
+          <ListItem active={router.asPath === '/parties/5'}>
             <Avatar size="2" shape="square" src="https://i.pravatar.cc/300" />
             <Text size={'2'}>파티 이름 입니다</Text>
           </ListItem>
