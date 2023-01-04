1a9d176f413228bea240542a887e68ff87be439a
jeonbyeongmin <qudals7613@gmail.com>
2023-01-05 03:51:00 +0900

    feat: change default dropdown style

--- components/primitive/Dropdown/Item.tsx
+++ components/primitive/Dropdown/Item.tsx
@@ -1,24 +1,37 @@
 import { Item as PrimitiveItem } from '@radix-ui/react-dropdown-menu';
-import { styled } from 'stitches.config';
+import { Flex } from 'components/primitive/Flex';
 
-export const Item = styled(PrimitiveItem, {
-  alignItems: 'center',
-  backgroundColor: 'transparent',
-  border: 0,
-  borderRadius: '$1',
-  color: 'inherit',
-  cursor: 'pointer',
-  display: 'flex',
-  fontSize: '$sm',
-  fontWeight: 500,
-  justifyContent: 'space-between',
-  lineHeight: 1,
-  outline: 'none',
-  padding: '$2 $3',
-  position: 'relative',
-  textAlign: 'left',
-  textDecoration: 'none',
-  userSelect: 'none',
-  width: '100%',
-  WebkitTapHighlightColor: 'transparent',
+import { ComponentProps, ElementRef, forwardRef, ReactNode } from 'react';
+import { CSS, styled } from 'stitches.config';
+
+type ItemVariants = ComponentProps<typeof CustomItem>;
+type ItemProps = ItemVariants & {
+  css?: CSS;
+  leftElement?: ReactNode;
+  rightElement?: ReactNode;
+};
+
+const CustomItem = styled(PrimitiveItem, {
+  p: '$5 $6',
+  br: '$md',
+  outline: '0',
+
+  '&:hover': {
+    backgroundColor: '$gray2',
+  },
 });
+
+export const Item = forwardRef<ElementRef<typeof CustomItem>, ItemProps>(
+  ({ children, leftElement, rightElement, ...props }, forwaredRef) => {
+    console.log(leftElement);
+    return (
+      <CustomItem ref={forwaredRef} {...props}>
+        <Flex align="center" gap={4}>
+          {!!leftElement ? leftElement : null}
+          {children}
+          {!!rightElement ? rightElement : null}
+        </Flex>
+      </CustomItem>
+    );
+  }
+);
