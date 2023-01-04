0874ba5ed916206c9bf288abb6627dbb50ce9d0d
jeonbyeongmin <qudals7613@gmail.com>
2023-01-05 00:28:22 +0900

    feat: dropdown primitive component 생성

---
+++ components/primitive/Dropdown/DropdownMenuGroup.tsx
@@ -0,0 +1,4 @@
+import { Group } from '@radix-ui/react-dropdown-menu';
+import { styled } from 'stitches.config';
+
+export const DropdownMenuGroup = styled(Group, {});
---
+++ components/primitive/Dropdown/DropdownMenuRadioGroup.tsx
@@ -0,0 +1,4 @@
+import { RadioGroup } from '@radix-ui/react-dropdown-menu';
+import { styled } from 'stitches.config';
+
+export const DropdownMenuRadioGroup = styled(RadioGroup, {});
