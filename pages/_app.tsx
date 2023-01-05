1a793a28f63f60ca9b9cccc44f7861bca82d7f59
jeonbyeongmin <qudals7613@gmail.com>
2023-01-05 15:03:35 +0900

    chore: next-nprogressbar에서 nprogress로 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
+import 'globalStyles.css';
+import NProgress from 'nprogress';
 import type { AppProps } from 'next/app';
 import { Noto_Sans } from '@next/font/google';
-import NextNProgress from 'nextjs-progressbar';
-import 'globalStyles.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+NProgress.configure({ showSpinner: false });
 
 const notoSans = Noto_Sans({
   weight: ['300', '400', '700'],
@@ -9,9 +13,25 @@ const notoSans = Noto_Sans({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleRouteStart);
+    router.events.on('routeChangeComplete', handleRouteDone);
+    router.events.on('routeChangeError', handleRouteDone);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteStart);
+      router.events.off('routeChangeComplete', handleRouteDone);
+      router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, []);
+
   return (
     <main className={notoSans.className}>
-      <NextNProgress />
       <Component {...pageProps} />
     </main>
   );
